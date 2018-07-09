ca9d5196ee018ebe0cb0ebd96a5674acecb5b507
Gustavo Rodrigues <gusttavorodriguesm@gmail.com>
2018-07-08 20:19:47 -0700

    Improved validator for user create

--- web-jsp/src/main/java/br/com/web/libraryJsp/validators/BeforeCreateUserValidator.java
+++ web-jsp/src/main/java/br/com/web/libraryJsp/validators/BeforeCreateUserValidator.java
@@ -1,19 +1,14 @@
 package br.com.web.libraryJsp.validators;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import br.com.web.libraryJsp.models.User;
-import br.com.web.libraryJsp.services.UserService;
 
-@Service("beforeCreateUserValidator")
+@Component("beforeCreateUserValidator")
 public class BeforeCreateUserValidator implements Validator {
-	
-	@Autowired
-	private UserService userService;
 
 	@Override
 	public boolean supports(Class<?> clazz) {
@@ -21,13 +16,19 @@ public class BeforeCreateUserValidator implements Validator {
 	}
 
 	@Override
-	public void validate(Object obj, Errors errors) {
-		User user = (User) obj;
-		
-		this.userService.save(user);
-
+	public void validate(Object target, Errors errors) {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "field.required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "field.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "passwordUncoded", "field.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "passwordUncodedConfirmation", "field.required");
 
+		User user = (User) target;
+
+		if (user.getPasswordUncoded() != null && user.getPasswordUncodedConfirmation() != null) {
+			if (user.getPasswordUncoded().length() < 7)
+				errors.rejectValue("passwordUncoded", "min.length");
+			else if (!user.getPasswordUncodedConfirmation().equals(user.getPasswordUncoded()))
+				errors.rejectValue("passwordUncodedConfirmation", "not.equals.password");
+		}
 	}
 }
