9ac4f186108a01884fd377cd4e57084c51f5af94
Gustavo Rodrigues <gusttavorodriguesm@gmail.com>
2018-07-11 09:25:56 -0700

    Enabling /oauth/check_token endpoint

--- api/src/main/java/com/api/library/security/AuthorizationServerConfiguration.java
+++ api/src/main/java/com/api/library/security/AuthorizationServerConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
@@ -33,6 +34,11 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
         endpointsConfigurer.tokenStore(this.tokenStore)
                 .authenticationManager(this.authenticationManager)
                 .userDetailsService(userDetailService);
+    }
+    
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+    	security.checkTokenAccess("permitAll()");
     }
     
     public void configure (ClientDetailsServiceConfigurer clients) throws Exception{
