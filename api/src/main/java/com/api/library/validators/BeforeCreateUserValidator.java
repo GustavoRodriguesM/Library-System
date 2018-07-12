d25e70a9adeb5818417406449188330d4917db3d
Gustavo Rodrigues <gusttavorodriguesm@gmail.com>
2018-07-12 09:41:08 -0700

    Removed Handler layer / Services are now injected on validators

--- api/src/main/java/com/api/library/validators/BeforeCreateUserValidator.java
+++ api/src/main/java/com/api/library/validators/BeforeCreateUserValidator.java
@@ -1,15 +1,20 @@
 package com.api.library.validators;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.api.library.models.User;
+import com.api.library.services.UserService;
 
 @Component("beforeCreateUserValidator")
 public class BeforeCreateUserValidator implements Validator {
 
+	@Autowired
+	private UserService userService;
+	
 	@Override
 	public boolean supports(Class<?> clazz) {
 		return User.class.equals(clazz);
@@ -17,12 +22,14 @@ public class BeforeCreateUserValidator implements Validator {
 
 	@Override
 	public void validate(Object target, Errors errors) {
+		User user = (User) target;
+		
+		this.userService.save(user);
+		
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "field.required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "field.required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "field.required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "passwordConfirmation", "field.required");
-
-		User user = (User) target;
 
 		if (user.getPassword() != null && user.getPasswordConfirmation() != null) {
 			if (user.getPassword().length() < 7)
