34e365d0086b90b5e62bb3216a8849789c31e95a
Gustavo Rodrigues <gusttavorodriguesm@gmail.com>
2018-07-14 14:17:11 -0700

    Add @GeneratedValue on ID

--- api/src/main/java/com/api/library/models/Author.java
+++ api/src/main/java/com/api/library/models/Author.java
@@ -4,6 +4,8 @@ import java.util.Calendar;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -22,6 +24,7 @@ import lombok.Setter;
 public class Author {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
 	private String name;
