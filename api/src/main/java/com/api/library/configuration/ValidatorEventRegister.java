5f255df2f3c45124b107372bbeabda0e41120cb2
Gustavo Rodrigues <gusttavorodriguesm@gmail.com>
2018-07-14 09:18:30 -0700

    Change the EventRegister because the last one is not working with beforeSave event

--- api/src/main/java/com/api/library/configuration/ValidatorEventRegister.java
+++ api/src/main/java/com/api/library/configuration/ValidatorEventRegister.java
@@ -1,34 +1,46 @@
 package com.api.library.configuration;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.validation.Validator;
 
 @Configuration
-public class ValidatorEventRegister implements InitializingBean {
- 
+public class ValidatorEventRegister extends RepositoryRestConfigurerAdapter {
+
+    private static final List<String> EVENTS;
+
+    static {
+        List<String> events = new ArrayList<String>();
+        events.add("beforeCreate");
+        events.add("afterCreate");
+        events.add("beforeSave");
+        events.add("afterSave");
+        events.add("beforeLinkSave");
+        events.add("afterLinkSave");
+        events.add("beforeDelete");
+        events.add("afterDelete");
+        EVENTS = Collections.unmodifiableList(events);
+    }
+
     @Autowired
-    ValidatingRepositoryEventListener validatingRepositoryEventListener;
- 
-    @Autowired
-    private Map<String, Validator> validators;
- 
+    ListableBeanFactory beanFactory;
+
     @Override
-    public void afterPropertiesSet() throws Exception {
-        List<String> events = Arrays.asList("beforeCreate");
+    public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+        super.configureValidatingRepositoryEventListener(validatingListener);
+        Map<String, Validator> validators = beanFactory.getBeansOfType(Validator.class);
         for (Map.Entry<String, Validator> entry : validators.entrySet()) {
-            events.stream()
-              .filter(p -> entry.getKey().startsWith(p))
-              .findFirst()
-              .ifPresent(
-                p -> validatingRepositoryEventListener
-               .addValidator(p, entry.getValue()));
+            EVENTS.stream().filter(p -> entry.getKey().startsWith(p)).findFirst()
+                    .ifPresent(p -> validatingListener.addValidator(p, entry.getValue()));
         }
     }
+
 }
