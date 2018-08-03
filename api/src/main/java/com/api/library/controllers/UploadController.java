0c9cc7d886f34be4bcdfef67ad7e75ebd9ba8062
Gustavo Rodrigues Martinez <gusttavorodriguesm@gmail.com>
2018-08-03 15:14:31 -0700

    Add cover field on body of response

--- api/src/main/java/com/api/library/controllers/UploadController.java
+++ api/src/main/java/com/api/library/controllers/UploadController.java
@@ -3,6 +3,7 @@ package com.api.library.controllers;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
@@ -17,10 +18,11 @@ public class UploadController {
 	@Autowired
 	private UploadFileService uploadFileService;
 
-	@PostMapping("/upload/cover")
+	@PostMapping(value="/upload/cover", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<?> upload(@RequestAttribute("file") MultipartFile file) {
 		URI upload = this.uploadFileService.upload(file);
-		return ResponseEntity.created(upload).build();
+		String body = "{\"cover\" : \"" + upload.getPath() + "\"}";
+		return ResponseEntity.created(upload).body(body);
 	}
 
 }
